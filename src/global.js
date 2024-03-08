94eb9ced0fee3ea201b12fe32933713a8810f624
Vishwaraj <74901922+Vishwaraj@users.noreply.github.com>
2024-03-08 17:15:20 +0530

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,2 +1,3 @@
 //export const API = 'http://localhost:4000';
-export const API = `https://vishwaraj-hype-kicks.herokuapp.com`;
+// export const API = `https://vishwaraj-hype-kicks.herokuapp.com`;
+export const API = "https://hypekicks-backend.onrender.com";
